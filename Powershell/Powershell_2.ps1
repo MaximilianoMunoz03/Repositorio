9320f3678dcdc48b0df0ef6d7f1d62a0d01a8a5f
MaximilianoMunoz03 <98655269+MaximilianoMunoz03@users.noreply.github.com>
2022-03-05 11:05:14 -0600

    Delete Powershell directory

--- Powershell/Powershell_2.ps1
+++
@@ -1,7 +0,0 @@
-#Detener servicios de la Computadora
-$Path = Read-Host "Introduzca el la ubicacion del TXT"
-Get-ChildItem -Path $Path -Force
-#Obtenemos el status de los servicios
-Get-Content -Path $Path | Get-Service
-#Iniciamos todos los servicios dedl del txt
-Get-Content -Path $Path | Get-Service | Start-Service
